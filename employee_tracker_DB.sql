0b3cf6342c0462e068b563053e30dd08c224070d
tanyap <biddanda.tanya@gmail.com>
2020-06-30 17:22:33 -0700

    adds package.json, starting files, beginnings of server

---
+++ employee_tracker_DB.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE employee_tracker_DB;
+
+USE employee_tracker_DB;
+
+CREATE TABLE employees(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employees(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
